ccb2d12c7ffd6760dd053d31a35fe014a564508f
nada1221 <nada931221@gmail.com>
2022-10-21 11:08:31 +0900

    finish commit

--- gittest/src/App.js
+++ gittest/src/App.js
@@ -1,22 +1,33 @@
+import styled from 'styled-components';
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [current, setNumb] = useState(0);
+
+  const upNumb = () => {
+    setNumb(current + 1);
+  };
+  const downNumb = () => {
+    setNumb(current - 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>{current}</div>
+        <div className="contain">
+          <button className="contain_button" onClick={upNumb}>
+            {' '}
+            +{' '}
+          </button>
+          <button className="contain_button" onClick={downNumb}>
+            {' '}
+            -{' '}
+          </button>
+        </div>
       </header>
     </div>
   );
